1f7c24fb4686887d842be9cc610352e33ce06d43
sharknia <furychick0@gmail.com>
2024-11-29 00:52:23 +0900

    프록시 추가

--- main.py
+++ main.py
@@ -104,9 +104,16 @@ class Crawler:
 
     def fetch_html(self):
         try:
-            response = requests.get(self.target_url)
+
+            proxies = {
+                "http": "http://8.219.97.248:80",
+                "https": "http://8.219.97.248:80",
+            }
+
+            response = requests.get(self.target_url, proxies=proxies, timeout=10)
             response.raise_for_status()
             self.html = response.text
+            logger.info("HTML 가져오기 성공: %s", self.target_url)
         except requests.exceptions.RequestException as e:
             logger.error(f"HTML 가져오기 실패: {e}")
             return False
