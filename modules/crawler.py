753a61b37fee2716988a4201443c8847fb055682
sharknia <furychick0@gmail.com>
2024-11-29 17:27:26 +0900

    알구몬 크롤링 기능 개선: HTML 가져오기 로직 수정 및 프록시 사용 추가

--- modules/crawler.py
+++ modules/crawler.py
@@ -14,37 +14,55 @@ class Crawler:
         self.target_url_fmkorea = (
             f"https://www.fmkorea.com/index.php?mid=hotdeal&page=1"
         )
-        self.html = None
+        self.html_algumon = None
         self.products = []
+        self.proxies = [
+            "http://8.219.97.248:80",
+        ]
 
     def fetch_html(self):
         try:
+            # 알구몬 fetch
+            self.algumon_html = self.algumon_fetch()
 
-            proxies = {
-                "http": "http://8.219.97.248:80",
-                "https": "http://8.219.97.248:80",
-            }
-
-            response = requests.get(self.target_url_algumon, timeout=100)
-            if response.status_code == 403:
-                logger.warning("403 Forbidden: IP 차단, 프록시로 시도")
-                response = requests.get(
-                    self.target_url_algumon, proxies=proxies, timeout=100
-                )
-            response.raise_for_status()
-            self.html = response.text
-            logger.info("HTML 가져오기 성공: %s", self.target_url_algumon)
+            # fmkorea fetch
         except requests.exceptions.RequestException as e:
-            logger.error(f"HTML 가져오기 실패: {e}")
+            logger.error(f"알구몬 HTML 가져오기 실패: {e}")
             return False
         return True
 
+    def algumon_fetch(self):
+        try:
+            response = requests.get(self.target_url_algumon, timeout=100)
+            if response.status_code == 403:
+                logger.warning("403 Forbidden: IP 차단, 프록시로 시도")
+                for proxy in self.proxies:
+                    try:
+                        proxies = {"http": proxy, "https": proxy}
+                        response = requests.get(
+                            self.target_url_algumon, proxies=proxies, timeout=100
+                        )
+                        if response.status_code == 200:
+                            return response.text
+                    except requests.exceptions.RequestException as e:
+                        logger.error(f"다음 프록시 재시도: {e}")
+                        continue
+                # while문을 다 돌아도 200이 아니면 결국 가져오기 실패이므로 None 반환
+                logger.error("알구몬 프록시로도 가져오기 실패")
+                return None
+            else:
+                logger.info("알구몬 HTML 가져오기 성공: %s", self.target_url_algumon)
+                return response.text
+        except requests.exceptions.RequestException as e:
+            logger.error(f"알구몬 HTML 가져오기 실패: {e}")
+            return None
+
     # 알구몬의 데이터를 정리
     def parse_products_algumon(self):
-        if not self.html:
+        if not self.html_algumon:
             return []
 
-        soup = BeautifulSoup(self.html, "html.parser")
+        soup = BeautifulSoup(self.html_algumon, "html.parser")
         product_list = soup.find("ul", class_="product post-list")
         if not product_list:
             logger.warning("상품 리스트를 찾을 수 없습니다.")
