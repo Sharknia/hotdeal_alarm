1ca817a9ed68fef11cf102451b0f54d5ab6fde3e
sharknia <furychick0@gmail.com>
2024-11-29 16:10:18 +0900

    first run 확인 전역변수 제거

--- main.py
+++ main.py
@@ -13,26 +13,26 @@ def job():
         logger.error(f"에러: {e}")
 
 
-def run_scheduled_tasks():
-    global first_run
+def run_scheduled_tasks(first_run):
     now = datetime.now()
 
     # 최초 실행 시 작업
     if first_run:
         logger.info(f"최초 실행 작업 실행: {now}")
         job()
-        first_run = False  # 최초 실행 이후 플래그 변경
-        return
+        return False  # 최초 실행 이후 플래그 변경
 
     # 매시 정시와 30분에 작업 실행
     elif now.minute % 30 <= 5:
         logger.info(f"정기 작업 실행: {now}")
         job()
 
+    return first_run
+
 
 if __name__ == "__main__":
     first_run = True  # 최초 실행 플래그
     logger.info("프로그램 시작")
     while True:
-        run_scheduled_tasks()
+        first_run = run_scheduled_tasks(first_run)
         time.sleep(300)  # 5분마다 확인
